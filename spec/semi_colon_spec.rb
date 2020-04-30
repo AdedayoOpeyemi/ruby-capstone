6beb26996834b0ccf7820da197d580712a6131bf
A L V A R O <alvarop3000@gmail.com>
2020-04-29 21:52:05 -0500

    write test for semi-colon file

---
+++ spec/semi_colon_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/semi_colon.rb'
+require_relative '../lib/errors.rb'
+
+RSpec.describe SemiColon do
+
+include SemiColon
+
+  describe '#semi_colon_message' do
+    it 'outputs an error message when there\'s an semi-colon missing at the end of a line' do
+      file = './css_test.css'
+      linter = Errors.new(file)
+      expect do
+        semi_colon_message(linter.args)
+      end.to output("\e[0;92;49mMissing semi-colon at the end of line 5\e[0m\n").to_stdout
+    end
+  end
+end
