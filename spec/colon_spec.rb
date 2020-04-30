c1364d25cc7d45d8918834abb0cf5df0cf71ac12
A L V A R O <alvarop3000@gmail.com>
2020-04-29 21:49:47 -0500

    write test for indent file

--- spec/colon_spec.rb
+++ spec/colon_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Colon do
 include Colon
 
   describe '#colon_space_message' do
-    it 'adds to an array if there\'s a missing space after a colon' do
+    it 'outputs an error message when there\'s a missing space in front of a colon' do
       file = './css_test.css'
       linter = Errors.new(file)
       expect do
-        p colon_space(linter.args)
-      end.to output("[\"Missing space after colon on line 3\"]\n").to_stdout
+        colon_space_message(linter.args)
+      end.to output("\e[0;91;49mMissing space after colon on line 3\e[0m\n").to_stdout
     end
   end
 end
---
+++ spec/indent_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/indent.rb'
+require_relative '../lib/errors.rb'
+
+RSpec.describe Indent do
+
+include Indent
+
+  describe '#bracket_message' do
+    it 'outputs an error message when there\'s an extra space in the beginning of a line' do
+      file = './css_test.css'
+      linter = Errors.new(file)
+      expect do
+        bracket_message(linter.args)
+      end.to output("\e[0;93;49mExtra space found at the beginning of line 7\e[0m\n").to_stdout
+    end
+  end
+end
